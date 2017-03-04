8294e1534828ee5667bf754c570ae0b13606209d
Philip Thomas <philiprobthomas@gmail.com>
2017-03-03 18:54:58 -0700

    Started adding framework for background scrolling

--- Background.h
+++ Background.h
@@ -11,6 +11,8 @@ private:
 	sf::Vector2f speed;
 	sf::Vector2f pos;
 	sf::Vector2f scale;
+	sf::Vector2f origin; //set size of window
+	bool orient; int iter;
 public:
 	Background(sf::Texture &backTexture, float nSpeed, float nPos[2], float scale);
 	void setSpeed(float nSpeed);
@@ -18,6 +20,19 @@ public:
 	void setScale(float nScale);
 	void main();
 	sf::Vector2f getSpeed();
+
+	void main()
+	{
+		if (orient && iter == 0)//if orient = 1, back 1 is on top
+		{
+			backVec[0].setPosition(0, 0);
+			backVec[1].setPosition(0, backVec[0].getSize()); //need to check if works
+		}
+		else if (!orient && iter == 0//if orient = 0, back 2 is on top
+		{
+
+		}
+	}
 };
 
 #endif
