3a80321713cbe7b697d0daae33e6934eee8a1b9f
Philip Thomas <philiprobthomas@gmail.com>
2017-03-10 18:50:46 -0700

    Scrolling in main of background added

--- Background.cpp
+++ Background.cpp
@@ -19,3 +19,43 @@ void Background::setPos(float nPos[2])
 {
 	pos = sf::Vector2f(nPos[1], nPos[2]);
 }
+void Background::setSpeed(float nSpeed)
+{
+	speed.y = nSpeed;
+}
+void Background::setScale(float nScale)
+{
+	scale.x = nScale;
+	scale.y = nScale;
+	for (std::vector<sf::Sprite>::iterator i = backVec.begin(); i != backVec.end(); ++i)
+	{
+		i->setScale(scale.x, scale.y);
+	}
+}
+void Background::flipOrient()
+{
+	orient *= -1;
+}
+sf::Vector2f Background::getSpeed()
+{
+	return speed;
+}
+
+void Background::main()
+{
+	if (orient && iter == 0)//if orient = 1, back 0 is on top
+	{
+		backVec[0].setPosition(0, 0);
+		backVec[1].setPosition(0, backVec[0].getGlobalBounds().height); //need to check if works
+	}
+	else if (!orient && iter == 0)//if orient = 0, back 1 is on top		
+	{
+		backVec[1].setPosition(0, 0));
+		backVec[0].setPosition(0, backVec[1].getGlobalBounds().height);
+	}
+	else
+	{
+		backVec[0].move(speed);
+		backVec[1].move(speed);
+	}
+}
